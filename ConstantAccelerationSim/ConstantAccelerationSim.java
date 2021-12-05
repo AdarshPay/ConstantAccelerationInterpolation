5a6ec170d274bdc52965fa2b8014cecbabe508bb
AdarshPay <46230966+AdarshPay@users.noreply.github.com>
2021-12-04 20:53:01 -0700

    Working simulation!

--- ConstantAccelerationSim/ConstantAccelerationSim.java
+++ ConstantAccelerationSim/ConstantAccelerationSim.java
@@ -1,6 +1,8 @@
 import org.json.JSONArray;
 import org.json.JSONML;
 import org.json.simple.JSONObject;
+
+import java.lang.ref.Cleaner;
 import java.util.Timer;
 import java.util.concurrent.TimeUnit;
 
@@ -21,123 +23,178 @@ public class ConstantAccelerationSim {
     static double currentX = 0.0;
     static double currentY = 0.0;
 
+    static double currentVelocityX = 0;
+    static double currentVelocityY = 0;
+    static double currentThetaChange = 0;
+
     static double currentTheta = 0.0;
     static double thetaChange = 0.0;
 
-    static Boolean inCurve = false;
-    static Boolean needCurve = false;
+    static double t1Theta = 0;
+
+    static double timingResolution = 50;
+
+    static double[] velocityArray = new double[3];
+
+    public static double[] constantAccelerationInterpolation(double currentX, double currentXVelocity, double currentY, double currentYVelocity, double currentTheta, double currentThetaVelocity, double time, JSONArray pathPointsJSON) {
+
+        org.json.JSONObject currentPoint;
+        org.json.JSONObject nextPoint;
+        org.json.JSONObject previousPoint;
+
+        double[] returnArray = new double[3];
+        int currentPointIndex = 0;
+        double lowestTimeDiff = 0;
+
+        double[] timeDiffArray = new double[pathPointsJSON.length()];
+
+        for(int i = 0; i < pathPointsJSON.length(); i++) {
+            timeDiffArray[i] = Math.abs(time - pathPointsJSON.getJSONObject(i).getDouble("Time"));
+        }
+        for(int i = 0; i < timeDiffArray.length; i++) {
+            if(i == 0) {
+                lowestTimeDiff = timeDiffArray[0];
+                currentPointIndex = 0;
+            }
+            else {
+                if(timeDiffArray[i] < lowestTimeDiff) {
+                    lowestTimeDiff = timeDiffArray[i];
+                    currentPointIndex = i;
+                }
+            }
+        }
+
+        currentPoint = pathPointsJSON.getJSONObject(currentPointIndex);
+
+        double currentPointTime = currentPoint.getDouble("Time");
+
+        double velocityX = 0;
+        double velocityY = 0;
+        double thetaChange = 0;
+
+        if(currentPointIndex + 1 >= pathPointsJSON.length()) {
+            currentPoint = pathPointsJSON.getJSONObject(currentPointIndex);
+            previousPoint = pathPointsJSON.getJSONObject(currentPointIndex - 1);
+            velocityX = (currentPoint.getDouble("X") - currentX)/(currentPointTime - time);
+            velocityY = (currentPoint.getDouble("Y") - currentY)/(currentPointTime - time);
+            thetaChange = (currentPoint.getDouble("Theta") - currentTheta)/(currentPointTime - time);
+            returnArray[0] = velocityX;
+            returnArray[1] = velocityY;
+            returnArray[2] = thetaChange;
+
+            return returnArray;
+        }
+        else if(currentPointIndex - 1 < 0) {
+            currentPoint = pathPointsJSON.getJSONObject(currentPointIndex);
+            nextPoint = pathPointsJSON.getJSONObject(currentPointIndex + 1);
+            double nextPointTime = nextPoint.getDouble("Time");
+            velocityX = (nextPoint.getDouble("X") - currentX)/(nextPointTime - time);
+            velocityY = (nextPoint.getDouble("Y") - currentY)/(nextPointTime - time);
+            thetaChange = (nextPoint.getDouble("Theta") - currentTheta)/(nextPointTime - time);
+            returnArray[0] = velocityX;
+            returnArray[1] = velocityY;
+            returnArray[2] = thetaChange;
+
+            return returnArray;
+        }
+
+        nextPoint = pathPointsJSON.getJSONObject(currentPointIndex + 1);
+        previousPoint = pathPointsJSON.getJSONObject(currentPointIndex - 1);
+
+        double nextPointTime = nextPoint.getDouble("Time");
+        double previousPointTime = previousPoint.getDouble("Time");
+
+        double timeDiffT1 = (currentPointTime - previousPointTime);
+        // double timeTonextPoint = (nextPointTime - time);
+        double t1 = (timeDiffT1 * turnTimePercent) + previousPointTime;
+
+        double timeDiffT2 = nextPointTime - currentPointTime;
+        double t2 = (timeDiffT2 * (1 - turnTimePercent)) + currentPointTime;
+
+        if(time <= t1) {
+            velocityX = (currentPoint.getDouble("X") - currentX)/(currentPointTime - time);
+            velocityY = (currentPoint.getDouble("Y") - currentY)/(currentPointTime - time);
+            thetaChange = (currentPoint.getDouble("Theta") - currentTheta)/(currentPointTime - time);
+        }
+        else if(time > t1 && time < t2) {
+            double t1X = currentXVelocity;
+            double t1Y = currentYVelocity;
+            double t1Theta = currentThetaVelocity;
+
+            double t2X = (nextPoint.getDouble("X") - currentPoint.getDouble("X"))/timeDiffT2;
+            double t2Y = (nextPoint.getDouble("Y") - currentPoint.getDouble("Y"))/timeDiffT2;
+            double t2Theta = (nextPoint.getDouble("Theta") - currentPoint.getDouble("Theta"))/timeDiffT2;
+
+            double previousVelocityX = t1X;
+            double previousVelocityY = t1Y;
+            double previousTheta = t1Theta;
+
+            double accelX = (t2X - t1X)/(t2 - time);
+            double accelY = (t2Y - t1Y)/(t2 - time);
+            double accelTheta = (t2Theta - t1Theta)/(t2 - time);
+
+            velocityX = (accelX * (1/timingResolution)) + previousVelocityX;
+            velocityY = (accelY * (1/timingResolution)) + previousVelocityY;
+            thetaChange = (accelTheta * (time - t1)) + previousTheta;
+        }
+        else if(time >= t2) {
+            velocityX = (nextPoint.getDouble("X") - currentX)/(nextPointTime - time);
+            velocityY = (nextPoint.getDouble("Y") - currentY)/(nextPointTime - time);
+            thetaChange = (nextPoint.getDouble("Theta") - currentTheta)/(nextPointTime - time);
+        }
+
+        returnArray[0] = velocityX;
+        returnArray[1] = velocityY;
+        returnArray[2] = thetaChange;
+
+        return returnArray;
+    }
 
     public static void main(String[] args) {
-        
-        for(int i = 0; i <= 7; i++) {
-            if(i == 2) {
-                point.put("X", 4.0);
-                point.put("Y", 1.0);
-                point.put("Theta", i * 18);
-                point.put("Time", (i * 10) -2);
-                pathPointsJSON.put(point);
-            }
-            else {
-                point.put("X", i * 2.0);
-                point.put("Y", i * 2.0);
-                point.put("Theta", i * 18);
-                point.put("Time", i * 10);
-                pathPointsJSON.put(point);
-            }
-            if(i > 5) {
-                point.put("X", 10.0);
-                point.put("Y", 10.0);
-                point.put("Theta", i * 18);
-                point.put("Time", i * 10);
-                pathPointsJSON.put(point);
-            }
-            
-        }
-        System.out.println(pathPointsJSON.toString());
+        point.put("X", 0.0);
+        point.put("Y", 0.0);
+        point.put("Theta", 0);
+        point.put("Time", 0);
+        pathPointsJSON.put(point);
+        point.put("X", 0.0);
+        point.put("Y", 0.0);
+        point.put("Theta", 0);
+        point.put("Time", 5);
+        pathPointsJSON.put(point);
+        point.put("X", 5.0);
+        point.put("Y", 0.0);
+        point.put("Theta", 0);
+        point.put("Time", 10);
+        pathPointsJSON.put(point);
+        point.put("X", 5.0);
+        point.put("Y", 5.0);
+        point.put("Theta", 0);
+        point.put("Time", 15);
+        pathPointsJSON.put(point);
 
-        org.json.JSONObject currentPoint = pathPointsJSON.getJSONObject(1);
-        org.json.JSONObject hookPoint = pathPointsJSON.getJSONObject(2);
-        org.json.JSONObject thirdPoint = pathPointsJSON.getJSONObject(3);
+        java.util.Random r = new java.util.Random();
+        // currentY = 1;
+        for(int i = 0; i < (pathPointsJSON.getJSONObject(pathPointsJSON.length() - 1).getDouble("Time")) * timingResolution; i++) {
+            double noiseX = r.nextGaussian() * Math.sqrt(0.0005);
+            double noiseY = r.nextGaussian() * Math.sqrt(0.0005);
+            double noiseTheta = r.nextGaussian() * Math.sqrt(0.0005);
 
-        for(int i = 0; i < 60; i++) {
-            if(inCurve == false) {
-                currentPoint = pathPointsJSON.getJSONObject(i/10);
-                hookPoint = pathPointsJSON.getJSONObject((i/10) + 1);
-                thirdPoint = pathPointsJSON.getJSONObject((i/10) + 2);
-            }           
-            int currentPointTime = currentPoint.getInt("Time");
-            int hookPointTime = hookPoint.getInt("Time");
-            int thirdPointTime = thirdPoint.getInt("Time");
+            noiseX = 0;
+            noiseY = 0;
+            noiseTheta = 0;
 
-            int timeDiffT1 = (hookPointTime - currentPointTime);
-            double t1 = (timeDiffT1 * turnTimePercent) + currentPointTime;
+            velocityArray = constantAccelerationInterpolation(currentX, currentVelocityX, currentY, currentVelocityY, currentTheta, currentThetaChange, (i/timingResolution), pathPointsJSON);
 
-            int timeDiffT2 = (thirdPointTime - hookPointTime);
-            double t2 = (timeDiffT2 * (1-turnTimePercent)) + hookPointTime;
+            currentVelocityX = velocityArray[0];
+            currentVelocityY = velocityArray[1];
+            currentThetaChange = velocityArray[2];
 
-            // System.out.println("T1: " + t1);
-            // System.out.println("T2: " + t2);
+            currentX = noiseX + currentX + ((1/timingResolution) * currentVelocityX);
+            currentY = noiseY + currentY + ((1/timingResolution) * currentVelocityY);
+            currentTheta = noiseTheta + currentTheta + ((1/timingResolution) * currentThetaChange);
 
-            // System.out.println("Current: " + currentPointTime + " Next: " + hookPointTime);
+            System.out.println(i/timingResolution + ", " + currentX + ", " + currentY + ", " + currentTheta + ", " + currentVelocityX + ", " + currentVelocityY + ", " + currentThetaChange);
 
-            double t1X = (hookPoint.getDouble("X") - currentPoint.getDouble("X"))/timeDiffT1;
-            double t1Y = (hookPoint.getDouble("Y") - currentPoint.getDouble("Y"))/timeDiffT1;
-
-            double t2X = (thirdPoint.getDouble("X") - hookPoint.getDouble("X"))/timeDiffT2;
-            double t2Y = (thirdPoint.getDouble("Y") - hookPoint.getDouble("Y"))/timeDiffT2;
-
-            if(t1X == t2X && t1Y == t2Y) {
-                needCurve = false;
-            }
-            else {
-                needCurve = true;
-            }
-
-            if(needCurve == false || i < t1) {
-                // replace currentPoint xy with odometry xy
-                velocityX = (hookPoint.getDouble("X") - currentPoint.getDouble("X"))/timeDiffT1;
-                // System.out.println("velocityX " + velocityX);
-                velocityY = (hookPoint.getDouble("Y") - currentPoint.getDouble("Y"))/timeDiffT1;
-
-                thetaChange = ((hookPoint.getDouble("Theta") - currentPoint.getDouble("Theta"))/timeDiffT1);
-
-                currentX = (velocityX * i) + originalX;
-                currentY = (velocityY * i) + originalY;
-
-                System.out.println("Time: " + i  + " VelocityX: " + velocityX + " VelocityY: " + velocityY + " AnglePerSec: " + thetaChange);
-            }
-            else {
-                needCurve = true;
-                // instead of calculating where it would be use odometry x and y
-                double t1Theta = ((hookPoint.getDouble("Theta")  - currentPoint.getDouble("Theta")) * ((t1 - 1)%timeDiffT1)/timeDiffT1) + currentPoint.getDouble("Theta");
-                double t2Theta = ((thirdPoint.getDouble("Theta") - hookPoint.getDouble("Theta")) * ((t2 -1)%timeDiffT2)/timeDiffT2) + hookPoint.getDouble("Theta");
-
-                System.out.println("Time: " + i + " t1Theta: " + t1Theta + " t2Theta: " + t2Theta);
-
-                double angleChangeRate = (t2Theta - t1Theta)/(t2 - t1);
-
-                double accelX = (t2X - t1X)/(t2 - t1);
-                double accelY = (t2Y - t1Y)/(t2 - t1);
-
-                // System.out.println("AccelX: " + accelX);
-
-                double adjustVelocityX = (accelX * (i - t1)) + velocityX;
-                double adjustVelocityY = (accelY * (i - t1)) + velocityY;
-
-                // if((Math.abs(adjustVelocityX - t2X) < 0.05 && Math.abs(adjustVelocityY - t2Y) < 0.05)) {
-                //     System.out.println("Inside finish");
-                //     // i = (int) t2;
-                //     needCurve = false;
-                // }
-
-                if((Math.abs(adjustVelocityX - t2X) == 0 && Math.abs(adjustVelocityY - t2Y) == 0)) {
-                    System.out.println("Inside finish");
-                    // i = (int) t2;
-                    needCurve = false;
-                }
-
-                System.out.println("Time: " + i  + " Hook time: " + t1 + " T2: " + t2 + " Adj Velocity X: " + adjustVelocityX + " Adj Velocity Y: " + adjustVelocityY + " AnglePerSec: " + angleChangeRate);
-            }
         }
     }
 
